d180601d27b17948e159d46d4dc37a3d7b250e5a
luis.jaar@dillards.com <luis.jaar@dillards.com>
2023-10-30 07:46:30 -0500

    added settings tabs and logout button in sidebar

--- myApp/src/pages/Menu.tsx
+++ myApp/src/pages/Menu.tsx
@@ -1,9 +1,9 @@
-import { IonContent, IonHeader, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import List from './List';
 import Settings from './Settings';
-import { cogOutline, homeOutline } from 'ionicons/icons';
+import { cogOutline, homeOutline, logOutOutline } from 'ionicons/icons';
 
 const Menu: React.FC = () => {
     const paths = [
@@ -13,28 +13,39 @@ const Menu: React.FC = () => {
   ]
   return (
     <IonPage>
-      <IonMenu contentId='main'>
-        <IonHeader>
-          <IonToolbar color={'secondary'}>
-            <IonTitle>Menu</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent className='ion-padding'>
-          {paths.map((item, index)=> (
-            <IonMenuToggle key={index}>
-              <IonItem routerLink={item.url} routerDirection='none'>{item.name}</IonItem>
+      <IonSplitPane contentId='main'>
+        <IonMenu contentId='main'>
+          <IonHeader>
+            <IonToolbar color={'secondary'}>
+              <IonTitle>Menu</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className='ion-padding'>
+            {paths.map((item, index)=> (
+              <IonMenuToggle key={index} autoHide={false}>
+                <IonItem detail={false} routerLink={item.url} routerDirection='none'>
+                  <IonIcon slot="start" icon={item.icon}/>
+                  {item.name}
+                  </IonItem>
+              </IonMenuToggle> 
+            ))}
+            <IonMenuToggle autoHide={false}>
+                <IonButton expand='full' routerLink='/' routerDirection='root'>
+                  <IonIcon slot="start" icon={logOutOutline}/>
+                  Logout
+                </IonButton>
             </IonMenuToggle> 
-          ))}
-        </IonContent>
-      </IonMenu>
+          </IonContent>
+        </IonMenu>
 
-      <IonRouterOutlet id='main'>
-        <Route path="/app/list" component={List}/>
-        <Route path="/app/settings" component={Settings}/>
-        <Route exact path="/app">
-          <Redirect to="/app/list"/>
-        </Route>
-      </IonRouterOutlet>
+        <IonRouterOutlet id='main'>
+          <Route path="/app/list" component={List}/>
+          <Route path="/app/settings" component={Settings}/>
+          <Route exact path="/app">
+            <Redirect to="/app/list"/>
+          </Route>
+        </IonRouterOutlet>
+      </IonSplitPane>
   </IonPage>
   );
 };
